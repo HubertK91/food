dd0cecc8119a778b9ea34a864f9418f30194fac2
bla <bla@bla.pl>
2024-06-14 22:14:37 +0200

    nowe6

--- src/main/resources/dataprod.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO client(firstname, lastname, city, phone , email, street_Address, username, password)
-VALUES('Jan','Kowalski', 'Warszawa', '888999123', 'jan@gmail.com', 'Słoneczna 12', 'admin', '{noop}admin');
-
-INSERT INTO client_role(user_id_client, role)
-VALUES (1, 'ROLE_ADMIN');
