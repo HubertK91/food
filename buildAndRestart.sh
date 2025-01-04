1ac2e6b0272c4cf646620110e9e1cb0fdfebc8a6
bla <bla@bla.pl>
2025-01-04 12:49:24 +0100

    aplikacja 04_01_2025_zmiany3

--- buildAndRestart.sh
+++ buildAndRestart.sh
@@ -1,4 +1,4 @@
 docker build -t food .
 docker stop food || true
 docker rm food || true
-docker run -d -p 8080:8080 --name food -e SPRING_PROFILES_ACTIVE=prod --network food-network --restart unless-stopped food
+docker run -d -p 8080:8080 --name=food -e SPRING_PROFILES_ACTIVE=prod --network food-network --restart unless-stopped food
