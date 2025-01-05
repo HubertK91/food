3770e04309ce98092853f276dec4c5bb1bf3c30c
bla <bla@bla.pl>
2025-01-05 12:25:19 +0100

    aplikacja 05_01_2025_zmiany

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,87 @@
+-- Dropping existing tables if they exist
+DROP TABLE IF EXISTS cart_items;
+DROP TABLE IF EXISTS client;
+DROP TABLE IF EXISTS client_order;
+DROP TABLE IF EXISTS client_role;
+DROP TABLE IF EXISTS dish;
+DROP TABLE IF EXISTS order_dishes;
+DROP TABLE IF EXISTS restaurant;
+DROP TABLE IF EXISTS restaurant_role;
+
+-- Creating the tables
+CREATE TABLE cart_items (
+                            id INT AUTO_INCREMENT PRIMARY KEY,
+                            quantity INT NOT NULL,
+                            client_id BIGINT,
+                            dish_id BIGINT,
+                            restaurant_id BIGINT,
+                            CONSTRAINT FK_cart_items_client FOREIGN KEY (client_id) REFERENCES client(id_client),
+                            CONSTRAINT FK_cart_items_dish FOREIGN KEY (dish_id, restaurant_id) REFERENCES dish(dish_id, restaurant_id)
+);
+
+CREATE TABLE client (
+                        id_client BIGINT AUTO_INCREMENT PRIMARY KEY,
+                        city VARCHAR(255) NOT NULL,
+                        email VARCHAR(255) NOT NULL,
+                        firstname VARCHAR(255) NOT NULL,
+                        lastname VARCHAR(255) NOT NULL,
+                        password VARCHAR(255),
+                        phone VARCHAR(255) NOT NULL,
+                        street_address VARCHAR(255) NOT NULL,
+                        username VARCHAR(255)
+);
+
+CREATE TABLE client_order (
+                              id_order BIGINT AUTO_INCREMENT PRIMARY KEY,
+                              id_client BIGINT,
+                              restaurant BIGINT,
+                              CONSTRAINT FK_client_order_client FOREIGN KEY (id_client) REFERENCES client(id_client),
+                              CONSTRAINT FK_client_order_restaurant FOREIGN KEY (restaurant) REFERENCES restaurant(id_restaurant)
+);
+
+CREATE TABLE client_role (
+                             id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                             role VARCHAR(255),
+                             user_id_client BIGINT,
+                             CONSTRAINT FK_client_role_client FOREIGN KEY (user_id_client) REFERENCES client(id_client)
+);
+
+CREATE TABLE dish (
+                      dish_id BIGINT NOT NULL,
+                      restaurant_id BIGINT NOT NULL,
+                      category VARCHAR(255),
+                      dish_name VARCHAR(255) NOT NULL,
+                      price DOUBLE,
+                      quantity INT,
+                      selected BOOLEAN DEFAULT FALSE NOT NULL,
+                      PRIMARY KEY (dish_id, restaurant_id),
+                      CONSTRAINT FK_dish_restaurant FOREIGN KEY (restaurant_id) REFERENCES restaurant(id_restaurant)
+);
+
+CREATE TABLE order_dishes (
+                              client_order_id BIGINT NOT NULL,
+                              dish_id BIGINT NOT NULL,
+                              restaurant_id BIGINT NOT NULL,
+                              PRIMARY KEY (client_order_id, dish_id, restaurant_id),
+                              CONSTRAINT FK_order_dishes_dish FOREIGN KEY (dish_id, restaurant_id) REFERENCES dish(dish_id, restaurant_id),
+                              CONSTRAINT FK_order_dishes_client_order FOREIGN KEY (client_order_id) REFERENCES client_order(id_order)
+);
+
+CREATE TABLE restaurant (
+                            id_restaurant BIGINT AUTO_INCREMENT PRIMARY KEY,
+                            category VARCHAR(255),
+                            city VARCHAR(255) NOT NULL,
+                            email VARCHAR(255) NOT NULL,
+                            name VARCHAR(255) NOT NULL,
+                            password VARCHAR(255),
+                            phone VARCHAR(255) NOT NULL,
+                            street_address VARCHAR(255) NOT NULL,
+                            username VARCHAR(255)
+);
+
+CREATE TABLE restaurant_role (
+                                 id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                                 role VARCHAR(255),
+                                 restaurant_id_restaurant BIGINT,
+                                 CONSTRAINT FK_restaurant_role_restaurant FOREIGN KEY (restaurant_id_restaurant) REFERENCES restaurant(id_restaurant)
+);
